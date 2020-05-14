14cf8ba9170754662051c3571616c7ffd856139c
SaifudinNA (ApinLight) <apin99lightning@gmail.com>
2020-05-14 08:17:38 +0700

    Change Keenam

--- src/Item_DaftarElektronik.java
+++ src/Item_DaftarElektronik.java
@@ -33,21 +33,24 @@ public class Item_DaftarElektronik extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         Item_TeksBarang = new javax.swing.JTextField();
         BackElektronik = new javax.swing.JButton();
-        lbJenisBarang = new javax.swing.JLabel();
         lbNamaBarang = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         Barang_Nama = new javax.swing.JLabel();
         Barang_Jenis = new javax.swing.JLabel();
         Barang_Jumlah = new javax.swing.JLabel();
+        lbJenisBarang = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(447, 302));
         setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Tempus Sans ITC", 0, 14)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Barang Elektronika");
         jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 450, -1));
 
         Item_Daftar.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "- - - Daftar Barang - - -", "Oven", "Kipas Angin", "Kulkas", "Dispenser" }));
         Item_Daftar.setToolTipText("");
@@ -56,6 +59,7 @@ public class Item_DaftarElektronik extends javax.swing.JFrame {
                 Item_DaftarActionPerformed(evt);
             }
         });
+        getContentPane().add(Item_Daftar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 279, -1));
 
         Item_DaftarOK.setText("OK");
         Item_DaftarOK.addActionListener(new java.awt.event.ActionListener() {
@@ -63,12 +67,15 @@ public class Item_DaftarElektronik extends javax.swing.JFrame {
                 Item_DaftarOKActionPerformed(evt);
             }
         });
+        getContentPane().add(Item_DaftarOK, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 30, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 510, -1, -1));
 
         Item_TeksBarang.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Item_TeksBarangActionPerformed(evt);
             }
         });
+        getContentPane().add(Item_TeksBarang, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 420, 370));
 
         BackElektronik.setText("Kembali");
         BackElektronik.addActionListener(new java.awt.event.ActionListener() {
@@ -76,6 +83,25 @@ public class Item_DaftarElektronik extends javax.swing.JFrame {
                 BackElektronikActionPerformed(evt);
             }
         });
+        getContentPane().add(BackElektronik, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 30, -1, -1));
+
+        lbNamaBarang.setText("Nama Barang ");
+        getContentPane().add(lbNamaBarang, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 100, -1));
+
+        jLabel4.setText("Jenis Barang");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 100, -1));
+
+        jLabel3.setText("Jumlah Tersedia");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 100, -1));
+
+        Barang_Nama.setText("-");
+        getContentPane().add(Barang_Nama, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 60, 310, -1));
+
+        Barang_Jenis.setText("-");
+        getContentPane().add(Barang_Jenis, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 80, 310, -1));
+
+        Barang_Jumlah.setText("-");
+        getContentPane().add(Barang_Jumlah, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 100, 310, -1));
 
         lbJenisBarang.setIcon(new javax.swing.ImageIcon(getClass().getResource("/istockphoto-648446306-1024x1024.jpg"))); // NOI18N
         lbJenisBarang.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
@@ -83,92 +109,39 @@ public class Item_DaftarElektronik extends javax.swing.JFrame {
                 lbJenisBarangPropertyChange(evt);
             }
         });
-
-        lbNamaBarang.setText("Nama Barang ");
-
-        jLabel4.setText("Jenis Barang");
-
-        jLabel3.setText("Jumlah Tersedia");
-
-        Barang_Nama.setText("-");
-
-        Barang_Jenis.setText("-");
-
-        Barang_Jumlah.setText("-");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 415, Short.MAX_VALUE)
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(Item_Daftar, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(Item_DaftarOK)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(BackElektronik, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addComponent(Item_TeksBarang, javax.swing.GroupLayout.DEFAULT_SIZE, 415, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(lbNamaBarang, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(28, 28, 28)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(Barang_Jenis)
-                            .addComponent(Barang_Nama)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(18, 18, 18)
-                        .addComponent(Barang_Jumlah)))
-                .addContainerGap(17, Short.MAX_VALUE))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(lbJenisBarang, javax.swing.GroupLayout.PREFERRED_SIZE, 448, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Item_Daftar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Item_DaftarOK)
-                    .addComponent(BackElektronik))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbNamaBarang)
-                    .addComponent(Barang_Nama))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(Barang_Jenis))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(Barang_Jumlah))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(12, 12, 12)
-                        .addComponent(jLabel2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(Item_TeksBarang, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(lbJenisBarang, javax.swing.GroupLayout.PREFERRED_SIZE, 384, Short.MAX_VALUE))
-        );
+        getContentPane().add(lbJenisBarang, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 450, 510));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void Item_DaftarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Item_DaftarActionPerformed
         // TODO add your handling code here:
+        if (Item_Daftar.getSelectedItem()=="- - - Daftar Barang - - -"){
+            Barang_Nama.setText("-");
+            Barang_Jenis.setText("-");
+            Barang_Jumlah.setText("-");
+        }
+        else if (Item_Daftar.getSelectedItem()=="Oven"){
+            Barang_Nama.setText("Oven Elsadiriti");
+            Barang_Jenis.setText("Elektronik Dapur");
+            Barang_Jumlah.setText("5");
+        }
+        else if (Item_Daftar.getSelectedItem()=="Kipas Angin"){
+            Barang_Nama.setText("Kipas Angin Sinshu");
+            Barang_Jenis.setText("Elektronik Keluarga");
+            Barang_Jumlah.setText("1");
+        }
+        else if (Item_Daftar.getSelectedItem()=="Kulkas"){
+            Barang_Nama.setText("Kulkas Freezering");
+            Barang_Jenis.setText("Elektronik Dapur");
+            Barang_Jumlah.setText("2");            
+        }
+        else if (Item_Daftar.getSelectedItem()=="Dispenser"){
+            Barang_Nama.setText("Dispenser Tirta");
+            Barang_Jenis.setText("Elektronik Dapur");
+            Barang_Jumlah.setText("3");
+        }
+        
     }//GEN-LAST:event_Item_DaftarActionPerformed
 
     private void BackElektronikActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackElektronikActionPerformed
@@ -179,12 +152,6 @@ public class Item_DaftarElektronik extends javax.swing.JFrame {
 
     private void Item_DaftarOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Item_DaftarOKActionPerformed
         // TODO add your handling code here:
-        if (Item_Daftar.getSelectedItem()=="- - - Daftar Barang - - -"){;}
-        else if (Item_Daftar.getSelectedItem()=="Oven"){;}
-        else if (Item_Daftar.getSelectedItem()=="Kipas Angin"){;}
-        else if (Item_Daftar.getSelectedItem()=="Kulkas"){;}
-        else if (Item_Daftar.getSelectedItem()=="Dispenser"){;}
-        
     }//GEN-LAST:event_Item_DaftarOKActionPerformed
 
     private void Item_TeksBarangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Item_TeksBarangActionPerformed
